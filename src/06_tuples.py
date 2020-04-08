62a7f42d89c88e5a0a9768361872ed6371d51bdc
Nicole Adams <nicoleadams.boston@gmail.com>
2020-04-08 12:51:44 -0400

    tuples and slices

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -37,12 +37,12 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # YOUR CODE HERE
 
 def print_tuple(x):
-    for s in range(len(x)):
-        print (x[s])
+    for s in x:
+        print (s)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
-print_tuple(u,)
+u = (1,)  # What needs to be added to make this work?
+print_tuple(u)
