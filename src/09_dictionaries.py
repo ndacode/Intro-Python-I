32ce597484d0eb14914acf79968270039be0a106
Nicole Adams <nicoleadams.boston@gmail.com>
2020-04-08 17:58:00 -0400

    add scopes

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -41,8 +41,6 @@ waypoints.append({"lat": 101.42, "lon" : 109.24, "name": "fourth and one"})
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 
-
-
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
